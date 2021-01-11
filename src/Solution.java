ccdc40e51fe0bbd263d0de290d93f05c518d7cda
Genpeng Xu <xgp1227@gmai.com>
2021-01-11 10:00:24 +0800

    add solution template

---
+++ src/Solution.java
@@ -0,0 +1,19 @@
+
+/**
+ * This is the solution of No. xxx problem in the LeetCode,
+ * the website of the problem is as follow:
+ * xxx
+ *
+ * The description of problem is as follow:
+ * ==========================================================================================================
+ * xxx
+ * ==========================================================================================================
+ *
+ * Difficulty: Easy or Medium or Hard
+ * Tags: xxx;xxx;
+ *
+ * @author Genpeng Xu (xgp1227atgmail.com)
+ */
+public class Solution {
+
+}
