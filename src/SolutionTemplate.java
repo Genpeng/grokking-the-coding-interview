cc18e6f601486bba87e7ecfc9227156c5d7241a6
Genpeng Xu <xgp1227@gmai.com>
2021-01-11 10:13:24 +0800

    modify solution template

---
+++ src/SolutionTemplate.java
@@ -0,0 +1,15 @@
+
+/**
+ * The description of problem is as follow:
+ * ==========================================================================================================
+ * xxx
+ * ==========================================================================================================
+ *
+ * Difficulty: Easy or Medium or Hard
+ * Tags: xxx;xxx;
+ *
+ * @author Genpeng Xu (xgp1227atgmail.com)
+ */
+public class SolutionTemplate {
+
+}
