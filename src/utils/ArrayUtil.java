04e603d64cb359d0d4d4393c4a7fbf436cd406ec
gerry.xu <gerry.xu@vipshop.com>
2021-10-06 06:56:56 +0800

    :memo: add ArrayUtil

---
+++ src/utils/ArrayUtil.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class ArrayUtil {
+    public static void swap(int[] arr, int i, int j) {
+        if (i != j) {
+            int tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
+    }
+}
