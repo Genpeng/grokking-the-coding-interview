9826c3afffa15255da1869c621af58d38545fc2f
xugenpeng <xgp1227@gmail.com>
2022-03-26 11:07:57 +0800

    :memo: add solution of linkedlist cycle

---
+++ src/pattern03_fast_n_slow_pointers/q01_linkedlist_cycle/Solution1.java
@@ -0,0 +1,28 @@
+package pattern03_fast_n_slow_pointers.q01_linkedlist_cycle;
+
+import entities.ListNode;
+
+/**
+ * The description of problem is as follow:
+ * ==========================================================================================================
+ * Given the head of a Singly LinkedList, write a function to determine if the LinkedList has a cycle in it or not.
+ * ==========================================================================================================
+ * <p>
+ * Difficulty: Easy
+ * Tags: linked list; fast & slow pointers
+ *
+ * @author Genpeng Xu (xgp1227atgmail.com)
+ */
+public class Solution1 {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
