74e5b6f7d4ad8e783a0fdb9edf7ab0408048ffea
xugenpeng <xgp1227@gmail.com>
2022-09-07 01:25:37 +0800

    :memo: modify some files

--- src/pattern04_merge_intervals/q03_insert_interval_leetcode0057/Solution1.java
+++ src/pattern04_merge_intervals/q03_insert_interval_leetcode0057/Solution1.java
@@ -1,6 +1,7 @@
 package pattern04_merge_intervals.q03_insert_interval_leetcode0057;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -66,5 +67,52 @@ public class Solution1 {
             ++i;
         }
         return result.toArray(new int[result.size()][2]);
+    }
+
+    public int[][] insertV2(int[][] intervals, int[] newInterval) {
+        // 如果区间列表为空，将插入的区间加到列表的末尾，并返回
+        if (intervals == null || intervals.length == 0) {
+            return new int[][] {{newInterval[0], newInterval[1]}};
+        }
+        // 如果插入的区间在列表的末尾（左边界大于列表最后一个区间的右边界），则将插入的区间添加到列表的末尾，并返回
+        final int N = intervals.length;
+        int L = newInterval[0], R = newInterval[1];
+        if (L > intervals[N-1][1]) {
+            int[][] result = new int[N+1][2];
+            for (int i = 0; i < N; ++i) {
+                result[i] = new int[] {intervals[i][0], intervals[i][1]};
+            }
+            result[N] = new int[] {L, R};
+            return result;
+        }
+        // 如果上述两种情况都不符合，找到需要插入的区间位置
+        List<int[]> result = new ArrayList<>();
+        int i = 0;
+        while (L > intervals[i][1]) {
+            result.add(new int[] {intervals[i][0], intervals[i][1]});
+            ++i;
+        }
+        // 合并重叠的区间
+        while (i < N & R >= intervals[i][0]) {
+            L = Math.min(L, intervals[i][0]);
+            R = Math.max(R, intervals[i][1]);
+            ++i;
+        }
+        result.add(new int[] {L, R});
+        // 添加剩余的区间
+        while (i < N) {
+            result.add(new int[] {intervals[i][0], intervals[i][1]});
+            ++i;
+        }
+        return result.toArray(new int[result.size()][2]);
+    }
+
+    public static void main(String[] args) {
+        Solution1 solu = new Solution1();
+//        int[][] result = solu.insertV2(new int[][] {{1, 3}, {6, 9}}, new int[] {2, 5}); // [[1,5],[6,9]]
+        int[][] result = solu.insertV2(new int[][] {{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}}, new int[] {4, 8}); // [[1,2],[3,10],[12,16]]
+        for (int[] interval : result) {
+            System.out.println(Arrays.toString(interval));
+        }
     }
 }
