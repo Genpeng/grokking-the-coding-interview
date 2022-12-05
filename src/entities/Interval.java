77082ec621ac90ed7407be3676433c209d713bbd
xugenpeng <xgp1227@gmail.com>
2022-12-05 20:37:38 +0800

    :memo: add Interval entity

---
+++ src/entities/Interval.java
@@ -0,0 +1,16 @@
+package entities;
+
+public class Interval {
+    public int start;
+    public int end;
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%s, %s)", this.start, this.end);
+    }
+}
